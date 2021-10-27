ae209fbe9561c8acb35ee364efb7110008b140b5
unaibrrgn <75972112+unaibrrgn@users.noreply.github.com>
2021-10-27 08:41:28 +0200

    Login sharedpreferences

--- app/src/main/java/com/example/retoandroid/MainActivity.java
+++ app/src/main/java/com/example/retoandroid/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.retoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -17,9 +19,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et_P = findViewById(R.id.eT_P);
         et_N = findViewById(R.id.eT_N);
+        setLogin("Admin","1234");
+
+
     }
     public void pInicial(View view) {
         Intent i = new Intent(this, Pantalla_Inicial.class );
         startActivity(i);
     }
+
+    public void setLogin(String usuario, String contraseña){
+
+
+        SharedPreferences preferences=getSharedPreferences("sharedprefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user",usuario);
+        editor.putString("pass",contraseña);
+    }
+
+    public String getLoginUsuario(){
+
+        SharedPreferences preferences=getSharedPreferences("sharedprefs", Context.MODE_PRIVATE);
+        String usuario = preferences.getString("user","");
+
+
+        return usuario;
+
+    }
+
+    public String getLoginContraseña(){
+
+        SharedPreferences preferences=getSharedPreferences("sharedprefs", Context.MODE_PRIVATE);
+        String contraseña = preferences.getString("pass","");
+
+
+        return contraseña;
+
+    }
+
+
 }
