86048063115ffe639a5e67cc8e30710c98048814
GorkaSagrario <75242711+GorkaSagrario@users.noreply.github.com>
2021-10-27 08:21:39 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/retoandroid/MainActivity.java
+++ app/src/main/java/com/example/retoandroid/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView et_N ;
+    private TextView et_P;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_P = findViewById(R.id.eT_P);
+        et_N = findViewById(R.id.eT_N);
     }
     public void pInicial(View view) {
         Intent i = new Intent(this, Pantalla_Inicial.class );
