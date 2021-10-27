c28b521586ebed40a2b4fb259466287844011eb2
GorkaSagrario <75242711+GorkaSagrario@users.noreply.github.com>
2021-10-27 08:55:38 +0200

    Pantalla_Inicio

--- app/src/main/java/com/example/retoandroid/Pantalla_Inicial.java
+++ app/src/main/java/com/example/retoandroid/Pantalla_Inicial.java
@@ -20,12 +20,12 @@ public class Pantalla_Inicial extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_inicial);
 
         nTarea = findViewById(R.id.eT_NTarea);
-        descripcion = findViewById(R.id.tv_descripcion);
+        descripcion = findViewById(R.id.etDescripcion);
         fecha = findViewById(R.id.et_fecha);
         coste = findViewById(R.id.eT_Coste);
 
         spinner =findViewById(R.id.spinner);
-        String []option={"urgente","alta","media","baja"};
+        String []option={"Urgente","Alta","Media","Baja"};
         ArrayAdapter<String> adapter = new
                 ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, option);
         spinner.setAdapter(adapter);
