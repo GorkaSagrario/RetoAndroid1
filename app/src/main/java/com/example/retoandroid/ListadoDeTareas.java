e16b885d535c1d5767f2070a3dd1fd2a88e6cc89
GorkaSagrario <75242711+GorkaSagrario@users.noreply.github.com>
2021-11-03 08:39:41 +0100

    LTareas

---
+++ app/src/main/java/com/example/retoandroid/ListadoDeTareas.java
@@ -0,0 +1,14 @@
+package com.example.retoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoDeTareas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_de_tareas);
+    }
+}
