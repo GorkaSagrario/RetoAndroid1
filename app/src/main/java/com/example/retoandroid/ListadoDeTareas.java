8c1669b5cb8e11148c188e6e71fbeb49547f361d
GorkaSagrario <75242711+GorkaSagrario@users.noreply.github.com>
2021-11-09 12:30:03 +0100

    menuEnListado

--- app/src/main/java/com/example/retoandroid/ListadoDeTareas.java
+++ app/src/main/java/com/example/retoandroid/ListadoDeTareas.java
@@ -3,6 +3,7 @@ package com.example.retoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class ListadoDeTareas extends AppCompatActivity {
 
@@ -11,4 +12,11 @@ public class ListadoDeTareas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_de_tareas);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuopciones, menu);
+        return true;
+    }
+
 }
