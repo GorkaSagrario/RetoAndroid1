8716393e32f410b62b211f11c9f7b63be92566bf
unaibrrgn <75972112+unaibrrgn@users.noreply.github.com>
2021-11-09 12:22:08 +0100

    Update AdminSQLiteOpenHelper.java

--- app/src/main/java/com/example/retoandroid/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/retoandroid/AdminSQLiteOpenHelper.java
@@ -1,7 +1,10 @@
 package com.example.retoandroid;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -26,6 +29,36 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper{
     public void onUpgrade(SQLiteDatabase db, int versionActual, int versionNueva) {
         db.execSQL(ELIMINAR_TABLA_TAREAS);
         db.execSQL(CREAR_TABLA_TAREAS);
+
+
+    }
+
+    public String fetchAllData(){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("select * from tareas", null, null);
+
+        String nombre = null;
+        String descripcion = null;
+        String fecha = null;
+        String coste = null;
+        String prioridad = null;
+        String estado = null;
+
+        while(c.moveToNext()){
+
+            nombre = c.getString(0);
+            descripcion = c.getString(1);
+            fecha = c.getString(2);
+            coste = c.getString(3);
+            prioridad = c.getString(4);
+            estado = c.getString(5);
+
+        }
+
+        return nombre;
+
+
 
 
     }
